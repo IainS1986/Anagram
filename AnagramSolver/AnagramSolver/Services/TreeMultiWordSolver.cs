011f686eb5c2f548458110c6548dc91307baf98b
Iain Stanford <i_stanford@hotmail.com>
2017-11-21 23:21:49 +0000

    Mulitword solving with the Tree system works, needs some code refactoring. Its a bit quicker than my previous effort as it handles (just) 10 letters, but still feel it could be a lot quicker.
    Ideally I need to try and make the tree parsing faster in some way....

--- AnagramSolver/AnagramSolver/Services/TreeMultiWordSolver.cs
+++ AnagramSolver/AnagramSolver/Services/TreeMultiWordSolver.cs
@@ -10,9 +10,13 @@ namespace AnagramSolver.Services
 {
     class TreeMultiWordSolver : IAnagramSolver
     {
+        bool LimitToSingleWord { get; set; }
+
         private TreeParser Tree { get; set; }
         public TreeMultiWordSolver(string filePath)
         {
+            LimitToSingleWord = false;
+
             Tree = new TreeParser();
             Tree.Parse(filePath);
 
@@ -28,29 +32,64 @@ namespace AnagramSolver.Services
             var upper = word.Replace(" ", string.Empty).ToUpper();
 
             //Recusively start searching the tree for anagrams for each possible starting char
-            for (int i=0; i<upper.Length; i++)
+            results.AddRange(StartScan(upper));
+
+            return results;
+        }
+
+        public List<string> StartScan(string chars)
+        {
+            List<string> results = new List<string>();
+
+            Dictionary<char, bool> scanned = new Dictionary<char, bool>();
+            for (int i = 0; i < chars.Length; i++)
             {
-                var root = Tree.Root(upper[i]);
-                results.AddRange(ScanTree(root, upper.Remove(i, 1)));
+                char c = chars[i];
+                if (scanned.ContainsKey(c))
+                    continue;
+
+                scanned.Add(c, true);
+                var root = Tree.Root(c);
+                results.AddRange(ScanTree(root, chars.Remove(i, 1)));
             }
 
-            return results.Distinct().ToList();
+            return results;
         }
 
         public List<string> ScanTree(TreeNode node, string chars)
         {
             List<string> results = new List<string>();
-            if (node.IsWord && string.IsNullOrEmpty(chars))
+            if(node.IsWord)
             {
                 string word = node.Word();
-                results.Add(word);
-            }
-            else if(chars.Length > 0)
-            {
-                //For each child in node that is in chars, recurse
-                for(int i=0; i<chars.Length; i++)
+                if(string.IsNullOrEmpty(chars))
                 {
-                    var child = node.GetChild(chars[i]);
+                    results.Add(word);
+                }
+                else if(!LimitToSingleWord)
+                {
+                    //Scan remaining 
+                    var subResults = StartScan(chars);
+                    if(subResults.Count > 0)
+                    {
+                        results.AddRange(subResults.Select(x => string.Format("{0} {1}", word, x)));
+                    } 
+                }
+            }
+
+            if (chars.Length > 0)
+            {
+                Dictionary<char, bool> scanned = new Dictionary<char, bool>();
+                //For each child in node that is in chars, recurse
+                for (int i=0; i<chars.Length; i++)
+                {
+                    char c = chars[i];
+
+                    if (scanned.ContainsKey(c))
+                        continue;
+
+                    scanned.Add(c, true);
+                    var child = node.GetChild(c);
                     if(child!= null)
                     {
                         results.AddRange(ScanTree(child, chars.Remove(i, 1)));
